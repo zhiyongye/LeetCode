a132854967ae5d3a1c9b71262bca2d1191f697de
zhiyong <zhiyong_ncu@163.com>
2018-05-11 16:02:08 +0800

    No.350 Intersection of Two Arrays II

--- Using-Hash-Table/intersection-of-two-arrays-II/main.cpp
+++ Using-Hash-Table/intersection-of-two-arrays-II/main.cpp
@@ -22,3 +22,25 @@ public:
         return result;  
     }
 };
+
+// 用哈希表
+// 时间复杂度: O(len(nums1)+len(nums2))
+// 空间复杂度: O(len(nums1))
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_map<int, int> record;
+        for( int i = 0 ; i < nums1.size() ; i ++ )
+            record[nums1[i]] += 1;
+
+        vector<int> resultVector;
+        for(int i = 0; i < nums2.size(); i ++)
+            if(record[ nums2[i] ] > 0){
+                resultVector.push_back(nums2[i]);
+                record[nums2[i]] --;
+            }
+
+        return resultVector;
+    }
+};
