2cf71a05aaf16854bd43c616cae6e0f3386ec39e
zhiyong <zhiyong_ncu@163.com>
2018-05-11 16:55:52 +0800

    454. 4Sum II

---
+++ Using-Hash-Table/4Sum-II/main.cpp
@@ -0,0 +1,27 @@
+// 454. 4Sum II
+// https://leetcode.com/problems/4sum-ii/description/
+// 时间复杂度: O(n^2)
+// 空间复杂度: O(n^2)
+
+class Solution {
+public:
+    int fourSumCount(vector<int>& A, vector<int>& B, vector<int>& C, vector<int>& D) {
+        unordered_map<int, int> record;
+		for (int i=0; i<C.size(); ++i) {
+			for (int j=0; j<D.size(); ++j) {
+				record[C[i]+D[j]]++;
+			}
+		}
+		
+		int res = 0;
+		for (int i=0; i<A.size(); ++i) {
+			for (int j=0; j<B.size(); ++j) {
+				if (record.find(0 - A[i] - B[j]) != record.end()){
+					res += record[0-A[i]-B[j]];
+				}
+			}
+		}
+		
+		return res;
+    }
+};
