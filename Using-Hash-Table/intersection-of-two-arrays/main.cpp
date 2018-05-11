b76f24cc56697c74907126cb03f322e95fc63526
zhiyong <zhiyong_ncu@163.com>
2018-05-11 16:03:24 +0800

    NO.349  Intersection of Two Arrays

--- Using-Hash-Table/intersection-of-two-arrays/main.cpp
+++ Using-Hash-Table/intersection-of-two-arrays/main.cpp
@@ -17,3 +17,21 @@ public:
         return vector<int>(resultSet.begin(), resultSet.end());  
     }
 };
+
+// 用哈希表
+// 时间复杂度: O(len(nums1)+len(nums2))
+// 空间复杂度: O(len(nums1))
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+
+        unordered_set<int> record(nums1.begin(), nums1.end());
+
+        unordered_set<int> resultSet;
+        for(int i = 0; i < nums2.size(); i ++ )
+            if(record.find(nums2[i]) != record.end())
+                resultSet.insert(nums2[i]);
+
+        return vector<int>(resultSet.begin(), resultSet.end());
+    }
+};
